df9dbb24873a88947ddd27449c46332ee3399bfb
arnoldas kurbanovas <akurbanovas@albany.edu>
2017-06-08 17:57:37 -0400

    updated to include full framework

--- raspPyRun.sh
+++ raspPyRun.sh
@@ -1,4 +1,6 @@
 #!/bin/bash  
+echo "Starting raspPy framework download"
+conda install git https://github.com/ASRCsoft/raspPy.git
 echo "Starting gcc and gfortran download"  
 conda install gcc
 echo "Starting xarray package download" 
